3041922202a0c52103595381fd58c899d0ccaabb
Jakub Jakacki <jakub.jakacki18@gmail.com>
2023-09-01 16:15:40 +0200

    Initial commit

---
+++ library/books.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+
+
+print(os.listdir())
+data = pd.read_csv('book.csv')
+
+
+def add(**kwargs):
+    print('')
+
+def addrembook(function,bookname):
+    print('')
+
+def add(bookname):
+    print('')
+
+def remove(bookname):
+    print('')
---
+++ library/main.py
@@ -0,0 +1,13 @@
+from books import *
+from client import *
+from rentbook import *
+
+#najpierw książki, klient, reszta
+
+def main_fun():
+    print(' ')
+
+
+if __name__ == '__main__':
+    main_fun()
+
